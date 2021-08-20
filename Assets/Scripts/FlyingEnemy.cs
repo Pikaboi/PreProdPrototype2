e217dbb803b0ca4b0e37490891c19f982aa929f8
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2021-08-20 14:07:23 +1200

    flying enemy done (for real)

--- Assets/Scripts/FlyingEnemy.cs
+++ Assets/Scripts/FlyingEnemy.cs
@@ -13,10 +13,15 @@ public class FlyingEnemy : Enemy
 
     private bool m_Patrolling = true;
 
+    private Pathfinding.AIDestinationSetter m_DestinationControl;
+    private Pathfinding.AIPath m_PathControl;
+
     // Start is called before the first frame update
     public override void Start()
     {
         m_agent = null;
+        m_PathControl = GetComponent<Pathfinding.AIPath>();
+        m_DestinationControl = GetComponent<Pathfinding.AIDestinationSetter>();
     }
 
     // Update is called once per frame
@@ -34,7 +39,7 @@ public class FlyingEnemy : Enemy
     private void Patrolling()
     {
         Collider[] collisions;
-        collisions = Physics.OverlapSphere(transform.position, 20.0f);
+        collisions = Physics.OverlapSphere(transform.position, 10.0f);
 
         foreach (Collider c in collisions)
         {
@@ -52,10 +57,24 @@ public class FlyingEnemy : Enemy
 
         Lookat2D();
 
+        float posToPlayer;
+        if (m_Player.transform.position.x - transform.position.x < 0.0f)
+        {
+            posToPlayer = 1.0f;
+        }
+        else
+        {
+            posToPlayer = -1.0f;
+        }
+
+        //Beyond Inelegant i'll fix it in Vert Slice
+        m_DestinationControl.target.gameObject.transform.position = m_Player.transform.position + new Vector3(5.0f * posToPlayer, 5.0f, 0.0f);
+
         if (m_currentBullet == null && m_shootTimer < 0.0f)
         {
             m_currentBullet = Instantiate(m_Bullet, transform.position + m_Aimer.transform.forward * 1.2f, transform.rotation);
-            m_currentBullet.GetComponent<EnemyBullet>().Fire(m_Aimer.transform.forward, m_agent.speed, 1.0f);
+            //Make the _PlayerSpeed 2x the speed of the AIs speed
+            m_currentBullet.GetComponent<EnemyBullet>().Fire(m_Aimer.transform.forward, m_PathControl.maxSpeed * 2.0f, 1.0f);
             m_shootTimer = 1.5f;
         }
     }
